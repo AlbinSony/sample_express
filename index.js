959636230a63368947804be731a65c2e8209e705
Albin Sony <albinsony2025@cs.sjcetpalai.ac.in>
2023-10-07 23:18:19 +0530

    sample code generated

--- index.js
+++ index.js
@@ -4,7 +4,19 @@ const port =3000;
 
 app.get('/', (req, res) => {
     res.send("<h1>HELoo<h1>")
-})
+});
+
+app.get('/about', (req, res) => {
+    res.send("About Me")
+});
+
+app.get('/contact', (req, res) => {
+    res.send("Contact")
+});
+
+app.get('/info', (req, res) => {
+    res.send("more info")
+});
 
 app.listen(port, () => {
     console.log(`The Sever Is Running At Port ${port}.`);
